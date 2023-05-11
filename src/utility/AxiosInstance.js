0c3a1500e24718e0c05f95e0a386fbeadce7ef18
zhukyu <phamtienhuu562002@gmail.com>
2023-05-12 00:18:28 +0700

    connect backend restaurants list

--- src/utility/AxiosInstance.js
+++ src/utility/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://foodngo-v1.onrender.com/api', // replace with your base URL
+    baseURL: 'https://foodngo-v2.onrender.com/api', // replace with your base URL
+    // baseURL: 'http://127.0.0.1:3001/api', // dev env
     headers: {
         'Content-Type': 'application/json',
         'Authorization':  `Bearer ${localStorage.getItem('access_token')}` // replace with your authorization token
