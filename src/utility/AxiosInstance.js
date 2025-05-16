28d9c2a6445e6ef18aaf2270cb72183972b2061e
huupt1904 <pham_tien_huu@itamiarts.co.jp>
2025-05-16 16:35:50 +0900

    update be url

--- src/utility/AxiosInstance.js
+++ src/utility/AxiosInstance.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	// baseURL: 'https://foodngo.vercel.app/api', // replace with your base URL
-	baseURL: 'https://foodngo-v2.onrender.com/api', // replace with your base URL
+    // baseURL: 'https://foodngo-v2.onrender.com/api', // replace with your base URL
 	// baseURL: 'http://127.0.0.1:3001/api', // dev env
+    baseURL: 'http://15.168.9.111/api',
 	headers: {
 		'Content-Type': 'application/json',
 		'Authorization': `Bearer ${localStorage.getItem('access_token')}` // replace with your authorization token
